a9e6752d3a813800bcdbea3f0348d4946e341d5a
yuhsuan <r07222045@ntu.edu.tw>
2024-02-03 20:43:29 +0800

    apply private methods

--- init_val_generator/tools/gaussian_image.py
+++ init_val_generator/tools/gaussian_image.py
@@ -4,46 +4,46 @@ import matplotlib.pyplot as plt
 
 class GaussianImage:
     def __init__(self, width, height, n = None, noise = True, random_seed = None, plot_mode = "none"):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
-        self.x = np.arange(width)
-        self.y = np.arange(height)
+        self.__x = np.arange(width)
+        self.__y = np.arange(height)
 
         np.random.seed(random_seed)
-        self.n = np.random.randint(5) + 1 if n is None else n
-        self.generate_random_parameters()
+        self.__n = np.random.randint(5) + 1 if n is None else n
+        self.__generate_random_parameters()
 
-        self.generate_gaussian_component()
+        self.__generate_gaussian_component()
         if plot_mode == "all":
-            self.plot_data("Model Data")
+            self.__plot_data("Model Data")
 
         if noise:
-            self.add_noise()
+            self.__add_noise()
             if plot_mode != "none":
-                self.plot_data("Original Data")
+                self.__plot_data("Original Data")
 
-    def generate_random_parameters(self):
+    def __generate_random_parameters(self):
 
         def random_gaussian_params():
             amp = np.random.uniform(0.4, 1) * np.random.choice([1, 1, -1])
-            center_x = np.random.uniform(self.width * 0.25, self.width * 0.75)
-            center_y = np.random.uniform(self.height * 0.25, self.height * 0.75)
-            x_sigma = np.random.uniform(self.width * 0.01, self.width * 0.2)
-            y_sigma = np.random.uniform(self.height * 0.01, self.height * 0.2)
+            center_x = np.random.uniform(self.__width * 0.25, self.__width * 0.75)
+            center_y = np.random.uniform(self.__height * 0.25, self.__height * 0.75)
+            x_sigma = np.random.uniform(self.__width * 0.01, self.__width * 0.2)
+            y_sigma = np.random.uniform(self.__height * 0.01, self.__height * 0.2)
             pa = np.random.uniform(0, 360)
             return [amp, center_x, center_y, x_sigma, y_sigma, pa]
 
         
-        self.model_components = []
-        for i in range(self.n):
-            self.model_components.append(random_gaussian_params())
+        self.__model_components = []
+        for i in range(self.__n):
+            self.__model_components.append(random_gaussian_params())
 
         param_format = 'amp: {:.4f}    center x: {:.4f}    center y: {:.4f}    fwhm x: {:.4f}    fwhm y: {:.4f}    pa: {:.4f}'
-        for i in range(self.n):
-            print("model    ", i, "    ", param_format.format(*self.model_components[i], sep = ", "))
+        for i in range(self.__n):
+            print("model    ", i, "    ", param_format.format(*self.__model_components[i], sep = ", "))
 
-    def generate_gaussian_component(self):
+    def __generate_gaussian_component(self):
 
         def get_gaussian_array(x, y, params):
             SQ_FWHM_TO_SIGMA = 1 / 8 / math.log(2)
@@ -73,21 +73,21 @@ class GaussianImage:
 
             return np.array(data)
 
-        self.data = np.zeros(self.width * self.height)
-        for i in range(self.n):
-            self.data += get_gaussian_array(self.x, self.y, self.model_components[i])
+        self.data = np.zeros(self.__width * self.__height)
+        for i in range(self.__n):
+            self.data += get_gaussian_array(self.__x, self.__y, self.__model_components[i])
 
-        self.data_x = np.tile(self.x, self.height)
-        self.data_y = np.repeat(self.y, self.width)
+        self.__data_x = np.tile(self.__x, self.__height)
+        self.__data_y = np.repeat(self.__y, self.__width)
 
-    def add_noise(self):
+    def __add_noise(self):
         noise_std = 0.1
-        noise = np.random.normal(0., noise_std, self.width * self.height)
+        noise = np.random.normal(0., noise_std, self.__width * self.__height)
 
         self.data += noise
 
-    def plot_data(self, title):
-        plt.imshow(np.resize(self.data, (self.height, self.width)), origin='lower', interpolation='nearest')
+    def __plot_data(self, title):
+        plt.imshow(np.resize(self.data, (self.__height, self.__width)), origin='lower', interpolation='nearest')
         plt.colorbar()
         plt.title(title)
         plt.show()
