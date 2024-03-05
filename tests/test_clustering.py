f7586fa989795feaa9a3fe20a87e9f25af87db4d
yuhsuan <r07222045@ntu.edu.tw>
2024-03-05 23:54:41 +0800

    add test for multiple peaks

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -4,7 +4,7 @@ from init_val_generator.clustering import k_means_plus_plus
 
 def test_k_means_plus_plus():
     # Generate sample data with a peak at (3, 2)
-    data_x = np.repeat(np.arange(8), 6)
+    data_x = np.tile(np.arange(8), 6)
     data_y = np.repeat(np.arange(6), 8)
     data = np.zeros((8 * 6,))
     peak_index = (3, 2)
@@ -21,3 +21,27 @@ def test_k_means_plus_plus():
     # Check the calculated centroid against the expected value
     assert centroids_x[0] == peak_index[0]
     assert centroids_y[0] == peak_index[1]
+
+
+def test_k_means_plus_plus_three_peaks():
+    # Generate sample data with peaks at (3, 2), (6, 5), and (1, 4)
+    data_x = np.tile(np.arange(8), 6)
+    data_y = np.repeat(np.arange(6), 8)
+    data = np.zeros((8 * 6,))
+    peak_indices = [(3, 2), (6, 5), (1, 4)]
+    amp = [1, 0.8, 0.5]
+    for i, peak_index in enumerate(peak_indices):
+        data[8 * peak_index[1] + peak_index[0]] = amp[i]
+
+    n_clusters = 3
+
+    centroids_x, centroids_y = k_means_plus_plus(data, data_x, data_y, n_clusters)
+
+    # Check if the centroids have correct shape
+    assert centroids_x.shape == (n_clusters,)
+    assert centroids_y.shape == (n_clusters,)
+
+    # Check the calculated centroids against the expected values
+    for i, peak_index in enumerate(peak_indices):
+        assert centroids_x[i] == peak_index[0]
+        assert centroids_y[i] == peak_index[1]
