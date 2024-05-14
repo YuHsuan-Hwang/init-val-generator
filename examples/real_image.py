3ed0f943979795307ce99f9d6663e036d8a60fea
yuhsuan <r07222045@ntu.edu.tw>
2024-05-14 14:39:42 +0800

    add example

--- examples/real_image.py
+++ examples/real_image.py
@@ -1,7 +1,7 @@
 import sys
 from astropy.io import fits
 import numpy as np
-from init_val_generator import guess
+from init_val_generator import InitValGenerator
 from init_val_generator.util import print_gaussian_param, plot_comparison
 
 
@@ -9,9 +9,18 @@ if len(sys.argv) < 2:
     sys.exit()
 
 image_data = fits.getdata(sys.argv[1])
-width = image_data.shape[0]
-height = image_data.shape[1]
+
+index = 0
+while image_data.shape[index] == 1:
+    index = index + 1
+height = image_data.shape[index]
+width = image_data.shape[index + 1]
+print(image_data.shape)
+
 image_data = np.reshape(image_data, width * height)
-estimates = guess(image_data, width, height, None)
+
+guesser = InitValGenerator("2-fwhm-estimate", "3-sigma")
+estimates = guesser.estimate(image_data, width, height, 3)
+
 print_gaussian_param(estimates)
 plot_comparison(image_data, width, height, [], estimates)
